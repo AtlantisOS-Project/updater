ef088267d0b7a8bf564f9a948398fcf8b40fb7e6
NachtsternBuild <hallo.schneller50kmh@gmail.com>
2025-09-06 12:04:37 +0200

    Update submodules
    (2025-09-06 12:04:37)

--- src/update-submodul.sh
+++ src/update-submodul.sh
@@ -6,14 +6,17 @@ cd "$(dirname "$0")" || exit 1
 
 echo "[INFO] Updating submodules..."
 git submodule update --remote --merge
+git add .
 
 # commit changes
-if ! git diff --quiet -- submodules; then
-    git add submodules
-    git commit -m "Update submodules" -m "($(date +'%Y-%m-%d %H:%M:%S'))"
-    git origin main
-    echo "[INFO] Submodule changes committed."
-else
-    echo "[INFO] No updates available."
+if output=$(git diff-index HEAD --name-only 2>&1); then
+    if [[ -n "$output" ]]; then
+    	git status
+    	sleep 4
+    	git commit -m "Update submodules" -m "($(date +'%Y-%m-%d %H:%M:%S'))"
+    	git origin main
+    	echo "[INFO] Submodule changes committed."
+	else
+    	echo "[INFO] No updates available."
+	fi
 fi
-
